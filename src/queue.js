54015156b71e3bbee4f2582f26652b944d42d453
Vika Lubenets <lubenetcvika@gmail.com>
2023-04-16 18:53:30 +0400

    feat: add part of queue task

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -15,14 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class Queue {
 
+  constructor(){
+    this.head = null;
+    this.length = 0;
+  }
+
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+
+    if(this.length === 0){
+      this.head = new ListNode(value);
+    } else {
+      let curItem = this.head;
+
+      while(curItem.next){
+        curItem = curItem.next;
+      }
+
+      curItem.next = new ListNode(value);
+    }
+    this.length++;
   }
 
   dequeue() {
