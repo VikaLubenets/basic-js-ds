d712e72867fc7f164476c3ce03c428a523716cf7
Vika Lubenets <lubenetcvika@gmail.com>
2023-04-16 17:22:44 +0400

    feat: add 4 methods for BST

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -9,23 +9,73 @@ const { NotImplementedError } = require('../extensions/index.js');
 class BinarySearchTree {
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.root = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this.root = addData(this.root, data);
+
+    function addData(node, data){
+
+      if(!node){
+        return new Node(data);
+      }
+
+      if(node.data === data){
+        return node;
+      }
+
+      if(data > node.data){
+        node.right = addData(node.right, data);
+      } else {
+        node.left = addData(node.left, data);
+      }
+
+      return node;
+    }
+
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchData(this.root, data);
+
+    function searchData(node, data){
+      if(!node){
+        return false;
+      }
+
+      if(node.data === data){
+        return true;
+      }
+
+      if(node.data < data){
+        return searchData(node.right, data);
+      } else {
+        return searchData(node.left, data);
+      }
+
+      
+    }
+    
+
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    return findNode(this.root, data);
+
+    function findNode(node, data){
+      if(!node){return null};
+
+      if(node.data === data){return node};
+
+      if(node.data > data){
+        return findNode(node.left, data);
+      } else {
+        return findNode(node.right, data);
+      }
+
+    }
+    
   }
 
   remove(/* data */) {
